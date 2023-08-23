88a66efef91637538fdf07d2d918f205a7072484
Utsadawut <utsadawut.sangthong@gmail.coom>
2023-08-24 04:59:19 +0700

    edit input 1

--- src/components/EditlistSchoolComponent.vue
+++ src/components/EditlistSchoolComponent.vue
@@ -108,7 +108,7 @@ export default {
   padding-right: 3rem;
   font-size: 1rem;
   line-height: 1.25rem;
-  width: 600px;
+  width: 100%;
   border-radius: 0.5rem;
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
 }
